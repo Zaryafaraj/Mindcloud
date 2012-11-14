e4f0fd12187322cb0d4936cdffbae0d352b2f576
fathalian <fatalian@uw.edu>
2012-11-14 00:52:26 -0800

    Added API Tests for Notes Endpoint

---
+++ Server/MindCloud/Tests/ApiTets/NotesAPITests.py
@@ -0,0 +1,39 @@
+__author__ = 'afathali'
+import urllib
+from tornado.ioloop import IOLoop
+from tornado.testing import AsyncHTTPTestCase
+from Tests.ApiTets.HTTPHelper import HTTPHelper
+from TornadoMain import Application
+
+__author__ = 'afathali'
+
+class NotesTests(AsyncHTTPTestCase):
+
+    account_id = '04B08CB7-17D5-493A-8ED1-E086FDC1327E'
+
+    def get_new_ioloop(self):
+        return IOLoop.instance()
+
+    def get_app(self):
+        application = Application()
+        return application
+
+    def test_save_note_with_file(self):
+
+        collection_name = 'collName'
+        params = {'collectionName':collection_name}
+        url = '/'+self.account_id + '/Collections'
+        response = self.fetch(path=url, method='POST', body=urllib.urlencode(params))
+        self.assertEqual(200, response.code)
+        note_file = open('../test_resources/note.xml')
+        url += '/' + collection_name + '/Notes'
+        params = {'noteName' : 'NoteName'}
+        headers, post_data = HTTPHelper.create_multipart_request_with_file_and_params\
+            (params, 'file', note_file)
+        response = self.fetch(path=url, headers=headers, method='POST',
+            body=post_data)
+        self.assertEqual(200, response.code)
+        #cleanup
+        url = '/'.join(['',self.account_id, 'Collections', collection_name])
+        self.fetch(path=url, method='DELETE')
+
