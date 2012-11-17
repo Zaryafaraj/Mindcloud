2d2c4968358c0cdd2e248cde99a5458fe4b203a7
fathalian <fatalian@uw.edu>
2012-11-16 22:42:27 -0800

    Added connections for sharingCollection in mongo

--- Server/MindCloud/Storage/DatabaseFactory.py
+++ Server/MindCloud/Storage/DatabaseFactory.py
@@ -10,6 +10,7 @@ class DatabaseFactory:
     __port = 27017
     __database_name = 'mindcloud'
     __accounts_collection_name = 'accounts'
+    __sharing_collection_name = 'sharings'
 
     @staticmethod
     def __get_accounts_db():
@@ -21,11 +22,26 @@ class DatabaseFactory:
         return DatabaseFactory.__accounts_db
 
     @staticmethod
-    def get_accounts_collection():
+    def __get_sharing_db():
+        if not hasattr(DatabaseFactory, '__sharing_db'):
+            DatabaseFactory.__sharing_db =\
+            asyncmongo.Client(pool_id='sharing_pool', host = DatabaseFactory.__host,
+                port= DatabaseFactory.__port, maxcached=10, maxconnections=100,
+                dbname=DatabaseFactory.__database_name)
+        return DatabaseFactory.__sharing_db
 
+    @staticmethod
+    def get_accounts_collection():
         db = DatabaseFactory.__get_accounts_db()
         collection_connection = db.connection(collectionname=DatabaseFactory.__accounts_collection_name)
         return collection_connection
+
+    @staticmethod
+    def get_sharing_collection():
+        db = DatabaseFactory.__get_sharing_db()
+        collection_connection = db.connection(collectionname=DatabaseFactory.__sharing_collection_name)
+        return collection_connection
+
 
 
 
