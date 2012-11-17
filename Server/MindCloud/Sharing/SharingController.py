d341f6487049c3b2a9709abf71a8df1f4c06ddc0
fathalian <fatalian@uw.edu>
2012-11-16 23:56:31 -0800

    Added save and read sharing record

--- Server/MindCloud/Sharing/SharingController.py
+++ Server/MindCloud/Sharing/SharingController.py
@@ -2,6 +2,9 @@ import os
 import random
 import string
 import uuid
+from tornado import gen
+from Sharing.SharingRecord import SharingRecord
+from Storage.DatabaseFactory import DatabaseFactory
 
 __author__ = 'afathali'
 
@@ -15,8 +18,32 @@ class SharingController:
         return ''.join(random.choice(chars) for x in range(SharingController.__SECRET_LENGTH))
 
     @staticmethod
-    def create_sharing_record(user_id, collection_name):
+    def create_sharing_record(user_id, collection_name, callback):
         sharing_secret = SharingController.generate_sharing_secret()
+        sharing_collection = DatabaseFactory.get_sharing_collection()
+        sharing_record = {SharingRecord.SECRET_KEY : sharing_secret,
+                          SharingRecord.OWNER_KEY : user_id,
+                          SharingRecord.COLLECTION_NAME_KEY : collection_name,
+                          SharingRecord.SUBSCIRBERS_KEY : [(user_id,collection_name)]}
+        yield gen.Task(sharing_collection.insert, sharing_record)
+        callback()
+
+    @staticmethod
+    def get_sharing_info(sharing_secret, callback):
+        sharing_collection = DatabaseFactory.get_sharing_collection()
+        query = {SharingRecord.SECRET_KEY : sharing_secret}
+        sharing_records_cursor = yield gen.Task(sharing_collection.find, query)
+        #if we have more sharing spaces with this sharing secret
+        #something is horribly wrong
+        assert sharing_records_cursor.count() == 1
+        sharing_record_bson = sharing_records_cursor[0]
+        sharing_record = SharingRecord(
+                        sharing_record_bson[SharingRecord.SECRET_KEY],
+                        sharing_record_bson[SharingRecord.OWNER_KEY],
+                        sharing_record_bson[SharingRecord.COLLECTION_NAME_KEY],
+                        sharing_record_bson[SharingRecord.SUBSCIRBERS_KEY])
+        callback(sharing_record)
+
 
 
 
---
+++ Server/MindCloud/Sharing/SharingRecord.py
@@ -0,0 +1,33 @@
+__author__ = 'afathali'
+
+class SharingRecord:
+
+    SECRET_KEY = 'secret'
+    OWNER_KEY = 'owner'
+    SUBSCIRBERS_KEY = 'subscribers'
+    COLLECTION_NAME_KEY = 'collection_name'
+
+    def __init__(self, sharing_secret, owner_id, collection_name, subscribers):
+        """
+        Args:
+            -``sharing_secret`` : 8 Byte secret ID
+            -``owner_id`` : the UUID belonging to the user who initiated the sharing
+            -``collection_name``: Name of the collection as it appears in the
+            owners account
+            - ``subscribers``: A list of (user_id, collection_name) .
+            The user_id is the id of the subscriber to the shared space
+            the collection_name is the name of the collection as it
+            appears in the subscribers account
+             """
+        self.sharing_secret = sharing_secret
+        self.owner_id = owner_id
+        self.collection_name = collection_name
+        self.subscribers = subscribers
+
+    def toDictionary(self):
+         return {SharingRecord.SECRET_KEY : self.sharing_secret,
+                          SharingRecord.OWNER_KEY : self.owner_id,
+                          SharingRecord.COLLECTION_NAME_KEY : self.owner_id,
+                          SharingRecord.SUBSCIRBERS_KEY : self.subscribers}
+
+
