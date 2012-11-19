18634daad1ad5d0b6bbef303b46c8d6c0a62519a
fathalian <fatalian@uw.edu>
2012-11-18 16:23:56 -0800

    Added existance check in sharinghandler

--- Server/MindCloud/Handlers/SharingHandler.py
+++ Server/MindCloud/Handlers/SharingHandler.py
@@ -4,6 +4,7 @@ from tornado import gen
 import tornado.web
 from Sharing.SharingController import SharingController
 from Storage.StorageResponse import StorageResponse
+from Storage.StorageServer import StorageServer
 
 __author__ = 'afathali'
 
@@ -14,15 +15,21 @@ class SharingHandler(tornado.web.RequestHandler):
     def post(self, user_id, collection_name):
 
         collection_name = urllib2.unquote(collection_name)
-        sharing_secret = yield gen.Task(SharingController.create_sharing_record,
-                                        user_id, collection_name)
-        if sharing_secret is None:
-            self.set_status(StorageResponse.SERVER_EXCEPTION)
-            self.finish()
+        does_exist = yield gen.Task(StorageServer.does_collection_exist,
+                                    user_id, collection_name)
+        if does_exist:
+            sharing_secret = yield gen.Task(SharingController.create_sharing_record,
+                                            user_id, collection_name)
+            if sharing_secret is None:
+                self.set_status(StorageResponse.SERVER_EXCEPTION)
+                self.finish()
+            else:
+                json_str = json.dumps({'sharing_secret':sharing_secret})
+                self.set_status(StorageResponse.OK)
+                self.write(json_str)
+                self.finish()
         else:
-            json_str = json.dumps({'sharing_secret':sharing_secret})
-            self.set_status(StorageResponse.OK)
-            self.write(json_str)
+            self.set_status(StorageResponse.NOT_FOUND)
             self.finish()
 
     @tornado.web.asynchronous
@@ -30,11 +37,17 @@ class SharingHandler(tornado.web.RequestHandler):
     def delete(self, user_id, collection_name):
 
         collection_name = urllib2.unquote(collection_name)
-        yield gen.Task(SharingController.remove_sharing_record,
-                        user_id,
-                        collection_name)
-        self.set_status(StorageResponse.OK)
-        self.finish()
+
+        does_exist = yield gen.Task(StorageServer.does_collection_exist,
+            user_id, collection_name)
+        if does_exist:
+            yield gen.Task(SharingController.remove_sharing_record,
+                            user_id,
+                            collection_name)
+            self.set_status(StorageResponse.OK)
+            self.finish()
+        else:
+            self.set_status(StorageResponse.NOT_FOUND)
 
 
 
