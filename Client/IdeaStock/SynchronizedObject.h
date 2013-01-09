0636c6e685c0ebced192ca5946be7120ddecad69
fathalian <fatalian@uw.edu>
2013-01-08 17:03:46 -0800

    Removed DropboxAssociativeBulletinBoard

---
+++ Client/IdeaStock/SynchronizedObject.h
@@ -0,0 +1,13 @@
+//
+//  SynchronizedObject.h
+//  Mindcloud
+//
+//  Created by Ali Fathalian on 1/8/13.
+//  Copyright (c) 2013 University of Washington. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SynchronizedObject <NSObject>
+
+@end
