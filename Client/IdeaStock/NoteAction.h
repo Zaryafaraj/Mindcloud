4b8ebf462cc80f7c3b4aa3800c84a93c2cbd6552
fathalian <fatalian@uw.edu>
2013-01-12 00:36:27 -0800

    Implemented Note Action

--- Client/IdeaStock/NoteAction.h
+++ Client/IdeaStock/NoteAction.h
@@ -11,5 +11,12 @@
 @interface NoteAction : MindcloudBaseAction
 
 typedef void (^get_note_callback)(NSData * noteData);
+typedef void (^delete_note_callback)(void);
 
+@property (nonatomic, strong) get_note_callback getCallback;
+@property (nonatomic, strong) delete_note_callback deleteCallback;
+
+-(id) initWithUserId: (NSString *) userID
+       andCollection: (NSString *) collectionName
+             andNote: (NSString *) noteName;
 @end
--- Client/IdeaStock/NoteAction.m
+++ Client/IdeaStock/NoteAction.m
@@ -10,5 +10,47 @@
 
 @implementation NoteAction
 
+-(id) initWithUserId: (NSString *) userID
+       andCollection: (NSString *) collectionName
+             andNote: (NSString *) noteName
+{
+    self = [super init];
+    NSString * resourcePath = [NSString stringWithFormat:@"%@/Collections/%@/Notes/%@", userID, collectionName, noteName];
+    
+    NSURL * url = [NSURL URLWithString:
+                   [self.baseURL stringByAppendingString:resourcePath]];
+    NSMutableURLRequest * theRequest = [NSMutableURLRequest requestWithURL:url
+                                        cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                           timeoutInterval:60.0];
+    self.request = theRequest;
+    return self;
+}
 
+- (void) connectionDidFinishLoading:(NSURLConnection *)connection
+{
+    [super connectionDidFinishLoading:connection];
+    
+    if ([self.request.HTTPMethod isEqualToString:@"GET"])
+    {
+        
+        if (self.lastStatusCode != 200 && self.lastStatusCode != 304)
+        {
+            NSLog(@"Received status %d", self.lastStatusCode);
+            self.getCallback(nil);
+            return;
+        }
+        
+        self.getCallback(self.receivedData);
+    }
+    else if ([self.request.HTTPMethod isEqualToString:@"DELETE" ])
+    {
+        if (self.lastStatusCode != 200 && self.lastStatusCode != 304)
+        {
+            NSLog(@"Received status %d", self.lastStatusCode);
+        }
+        
+        self.deleteCallback();
+        return;
+    }
+}
 @end
