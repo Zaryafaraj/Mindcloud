fbca1bd6628b1d333cb06378eefa0df525d755e6
fathalian <fatalian@uw.edu>
2013-04-06 15:26:52 -0700

    Main Page Boiler Background

---
+++ Lists/ListMainPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListMainPageViewController.h
+//  Lists
+//
+//  Created by Ali Fathalian on 4/6/13.
+//  Copyright (c) 2013 MindCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListMainPageViewController : UIViewController
+
+@end
---
+++ Lists/ListMainPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  ListMainPageViewController.m
+//  Lists
+//
+//  Created by Ali Fathalian on 4/6/13.
+//  Copyright (c) 2013 MindCloud. All rights reserved.
+//
+
+#import "ListMainPageViewController.h"
+
+@interface ListMainPageViewController ()
+
+@end
+
+@implementation ListMainPageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
