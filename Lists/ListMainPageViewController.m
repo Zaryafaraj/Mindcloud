e572329f9845020eb2c4dddacf4c44a8dabec0ad
fathalian <fatalian@uw.edu>
2013-04-06 22:19:13 -0700

    Added Toolbar

--- Lists/ListMainPageViewController.m
+++ Lists/ListMainPageViewController.m
@@ -7,10 +7,10 @@
 //
 
 #import "ListMainPageViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface ListMainPageViewController ()
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (strong, nonatomic) NSMutableArray * collections;
+@property (weak, nonatomic) IBOutlet UIView *toolbar;
 @end
 
 @implementation ListMainPageViewController
@@ -25,49 +25,19 @@
     return self;
 }
 
-#pragma mark - Table View Delegate/DataSource
--(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
--(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return [self.collections count];
-}
-
--(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"CollectionTitleCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier
-                                                            forIndexPath:indexPath];
-    cell.contentView.backgroundColor = [UIColor lightGrayColor];
-    cell.contentView.alpha = 0.5;
-    cell.textLabel.text = self.collections[indexPath.item];
-    return cell;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    self.collections = [NSMutableArray array];
-    [self.collections addObject:@"ALi"];
-    [self.collections addObject:@"Leila"];
-    [self.collections addObject:@"Three"];
-    [self.collections addObject:@"Four"];
-    self.tableView.dataSource = self;
-    self.tableView.delegate = self;
+    [self.toolbar.layer setShadowColor:[UIColor blackColor].CGColor];
+    [self.toolbar.layer setShadowOpacity:1.0];
+    self.toolbar.layer.opaque = YES;
+    self.toolbar.layer.shouldRasterize = YES;
+    [self.toolbar.layer setShadowRadius:3.0];
+    [self.toolbar.layer setShadowOffset:CGSizeMake(2.0, 2.0)];
 }
 
 -(void) viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
-    CGRect tableFrame = CGRectMake(self.tableView.frame.origin.x,
-                                   self.tableView.frame.origin.y,
-                                   self.tableView.frame.size.width,
-                                   2 * [self.collections count]);
-    self.tableView.frame = tableFrame;
 }
 @end
