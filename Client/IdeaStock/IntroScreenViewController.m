c155ad2dc159949813b76313f2affd0956160a0e
Ali Fathalian <fatalian@uw.edu>
2013-12-07 01:45:15 -0800

    Intro Screen With basic subviews

--- Client/IdeaStock/IntroScreenViewController.m
+++ Client/IdeaStock/IntroScreenViewController.m
@@ -12,7 +12,8 @@
 @interface IntroScreenViewController ()
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
-
+//indexed on page number starting from 0
+@property (strong, nonatomic) NSMutableDictionary * pageViews;
 @end
 
 @implementation IntroScreenViewController
@@ -28,18 +29,132 @@
 
 #define NUMBER_OF_PAGES 4
 #define VIEW_OFFSET 150
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.pageViews = [NSMutableDictionary dictionary];
     self.scrollView.backgroundColor = [[ThemeFactory currentTheme] tintColor];
     self.scrollView.delegate = self;
     UIView * contentView = [[UIView alloc] init];
+    contentView.contentMode = UIViewContentModeRedraw;
     contentView.backgroundColor = [UIColor clearColor];
     contentView.translatesAutoresizingMaskIntoConstraints = NO;
     [self.scrollView addSubview:contentView];
-    UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 50, 50)];
-    label.text = @"HI";
-    [contentView addSubview:label];
+    
+    
+    [self setScrollViewContent:contentView];
+    
+    [self setContentView:contentView];
+}
+
+-(void) setContentView:(UIView *) contentView
+{
+    
+    NSMutableArray * array = [NSMutableArray array];
+    NSMutableDictionary * dict = [NSMutableDictionary dictionary];
+    NSString * constraint = @"H:|-0-";
+    for (int i = 0 ; i < NUMBER_OF_PAGES; i++)
+    {
+        UIView * result = [self setPageContent: contentView withPageNumber:i];
+        [dict setObject:result
+                 forKey:[NSString stringWithFormat:@"view%d", i]];
+        result.translatesAutoresizingMaskIntoConstraints = NO;
+        [contentView addSubview:result];
+        NSNumber * index = [NSNumber numberWithInt:i];
+        [self.pageViews setObject:result
+                           forKey:index];
+        
+        CGFloat numOfPage = NUMBER_OF_PAGES;
+        NSLayoutConstraint * widthConstraint = [NSLayoutConstraint constraintWithItem:result
+                                                                            attribute:NSLayoutAttributeWidth
+                                                                            relatedBy:NSLayoutRelationEqual
+                                                                               toItem:contentView
+                                                                            attribute:NSLayoutAttributeWidth
+                                                                           multiplier:1/numOfPage
+                                                                             constant:0];
+        
+        NSLayoutConstraint * heightConstraint = [NSLayoutConstraint constraintWithItem:result
+                                                                            attribute:NSLayoutAttributeHeight
+                                                                            relatedBy:NSLayoutRelationEqual
+                                                                               toItem:contentView
+                                                                            attribute:NSLayoutAttributeHeight
+                                                                           multiplier:1
+                                                                             constant:0];
+        NSDictionary * viewBindings = NSDictionaryOfVariableBindings(result);
+        NSString * originContrainst = @"V:|[result]|";
+        NSArray * originConstraints = [NSLayoutConstraint constraintsWithVisualFormat:originContrainst
+                                                options:0 metrics:nil
+                                                  views:viewBindings];
+        [array addObject: widthConstraint];
+        [array addObject: heightConstraint];
+        [array addObjectsFromArray:originConstraints];
+        constraint = [constraint stringByAppendingString:[NSString stringWithFormat:@"[view%d]-0-", i]];
+    }
+    
+    constraint = [constraint stringByAppendingString:@"|"];
+    [contentView addConstraints: [NSLayoutConstraint constraintsWithVisualFormat:constraint
+                                                                                     options:0
+                                                                                     metrics:nil
+                                                                               views:dict]];
+    [contentView addConstraints:array];
+}
+
+-(UIView *) setPageContent:(UIView *) contentView
+        withPageNumber:(int) i
+{
+    
+    UIColor * color = [UIColor whiteColor];
+    if ( i == 0)
+    {
+        color = [UIColor yellowColor];
+    }
+    if ( i ==1 )
+    {
+        color = [UIColor greenColor];
+    }
+    if ( i ==2 )
+    {
+        color = [UIColor redColor];
+    }
+    if ( i == 3)
+    {
+        color = [UIColor blueColor];
+    }
+    
+    UIView * view = [[UIView alloc] init];
+    view.backgroundColor = color;
+    return view;
+//    UILabel * label1 = [[UILabel alloc] init];
+//    label1.text = [NSString stringWithFormat:@"%d", i];
+//    
+//    label1.font = [UIFont systemFontOfSize:72];
+//    label1.translatesAutoresizingMaskIntoConstraints = NO;
+//    
+//    [contentView addSubview:label1];
+//    NSLayoutConstraint * constraint1 = [NSLayoutConstraint constraintWithItem:label1
+//                                                                    attribute:NSLayoutAttributeCenterY
+//                                                                    relatedBy:NSLayoutRelationEqual
+//                                                                       toItem:contentView
+//                                                                    attribute:NSLayoutAttributeCenterY
+//                                                                   multiplier:1.f
+//                                                                     constant:0.f];
+//    
+//    CGFloat pageNum = NUMBER_OF_PAGES;
+//    NSLayoutConstraint * constraint2 = [NSLayoutConstraint constraintWithItem:label1
+//                                                                    attribute:NSLayoutAttributeCenterX
+//                                                                    relatedBy:NSLayoutRelationEqual
+//                                                                       toItem:contentView
+//                                                                    attribute:NSLayoutAttributeCenterX
+//                                                                   multiplier: 1 / pageNum
+//                                                                     constant:0.f + (i - 1)];
+//    
+//    [contentView addConstraints:@[constraint1, constraint2]];
+}
+
+-(void) setScrollViewContent:(UIView *) contentView
+{
+    
     NSLayoutConstraint * widthConstraint = [NSLayoutConstraint constraintWithItem:contentView
                                                                    attribute:NSLayoutAttributeWidth
                                                                    relatedBy:NSLayoutRelationEqual
@@ -68,8 +183,8 @@
                                                                                    metrics:nil
                                                                               views:views]];
     [self.scrollView addConstraints:@[heightConstraints, widthConstraint]];
-    
 }
+
 
 -(void) viewDidAppear:(BOOL)animated
 {
@@ -85,9 +200,21 @@
 -(void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
     // Update the page when more than 50% of the previous/next page is visible
+}
+
+-(void) scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    
     CGFloat pageWidth = self.scrollView.frame.size.width;
     int page = floor((self.scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
     self.pageControl.currentPage = page;
 }
-
+-(void) willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
+                                         duration:(NSTimeInterval)duration
+{
+    int pageNo = self.pageControl.currentPage;
+    CGFloat xOffset = pageNo * self.scrollView.frame.size.width;
+    
+    self.scrollView.contentOffset = CGPointMake(xOffset, self.scrollView.contentOffset.y);
+}
 @end
