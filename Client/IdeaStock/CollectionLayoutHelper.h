a8acfbb6183897ff00d098fc1db9f7a8d6a0a306
Ali Fathalian <fatalian@uw.edu>
2013-12-24 18:30:58 -0800

    Images resize correctly

--- Client/IdeaStock/CollectionLayoutHelper.h
+++ Client/IdeaStock/CollectionLayoutHelper.h
@@ -18,8 +18,8 @@
 #define EXPAND_COL_SIZE 5
 #define STACKING_SCALING_WIDTH 1.0
 #define STACKING_SCALING_HEIGHT 1.0
-#define NOTE_WIDTH 310
-#define NOTE_HEIGHT 220
+#define NOTE_WIDTH 350
+#define NOTE_HEIGHT 260
 #define STACK_WIDTH 310
 #define STACK_HEIGHT 220
 @interface CollectionLayoutHelper : NSObject
--- Client/IdeaStock/ImageNoteView.m
+++ Client/IdeaStock/ImageNoteView.m
@@ -19,9 +19,7 @@
 @interface ImageNoteView()
 
 @property UIImageView * imageView;
-@property UIView * placeHolderView;
-@property UIView * controlView;
-@property BOOL isControlViewShowing;
+@property UIView * noteView;
 
 @end
 
@@ -44,7 +42,9 @@
 {
     self.resizesToFitImage = YES;
     //find the text view
-    for (UIView * subView in self.subviews){
+    UIView * noteView = self.subviews.firstObject;
+    self.noteView = noteView;
+    for (UIView * subView in self.noteView.subviews){
         if ([subView isKindOfClass:[UIImageView class]])
         {
             self.imageView = (UIImageView *) subView;
@@ -88,10 +88,6 @@
 -(void) setText:(NSString *)text
 {
     [super setText:text];
-    if (![self._textView.text isEqualToString:@""])
-    {
-        [self showPlaceHolder];
-    }
 }
 
 -(void) setFrame:(CGRect)frame
@@ -117,226 +113,23 @@
     {
         self.imageView.frame = CGRectMake(0,
                                           0,
-                                          self.bounds.size.width,
-                                          self.bounds.size.height);
-    }
-    if (self.placeHolderView)
-    {
-        self.placeHolderView.frame = CGRectMake(0,
-                                                0,
-                                                self.bounds.size.width,
-                                                self.bounds.size.height);
-    }
-    //if there is a control view resize everything so that control view is at the bottom
-    if (self.isControlViewShowing)
-    {
-        self._textView.frame = CGRectMake(self._textView.frame.origin.x,
-                                          self._textView.frame.origin.y,
-                                          self._textView.bounds.size.width,
-                                          self._textView.bounds.size.height - EXTENDED_EDGE_SIZE);
-        
-        self.imageView.frame = CGRectMake(self.imageView.bounds.origin.x,
-                                          self.imageView.bounds.origin.y,
-                                          self.imageView.bounds.size.width,
-                                          self.imageView.bounds.size.height - EXTENDED_EDGE_SIZE);
-        
-        if (self.placeHolderView)
-        {
-            self.placeHolderView.frame = self.imageView.frame;
-        }
-        
-        CGRect controlViewFrame = CGRectMake(self.imageView.frame.origin.x,
-                                             self.imageView.frame.origin.y + self.imageView.frame.size.height,
-                                             self.imageView.frame.size.width,
-                                             EXTENDED_EDGE_SIZE);
-        self.controlView.frame = controlViewFrame;
-        [self layoutControlElements];
+                                          self.noteView.bounds.size.width,
+                                          self.noteView.bounds.size.height);
     }
 }
 
 -(instancetype) prototype
 {
     ImageNoteView * prototype = [[ImageNoteView alloc] initWithFrame:self.frame];
+    prototype = [super _configurePrototype:prototype];
     UIImageView * newImageView = [[UIImageView alloc] initWithFrame:self.imageView.frame];
-    [prototype addSubview:newImageView];
+    [prototype.noteView addSubview:newImageView];
     prototype.imageView = newImageView;
     newImageView.backgroundColor = self.imageView.backgroundColor;
     newImageView.alpha = self.imageView.alpha;
-    prototype = [super _configurePrototype:prototype];
+    [prototype configureImageView];
     prototype._textView.text = @"";
     return prototype;
-}
-
--(void) extendView
-{
-    CGRect extendedBounds = CGRectMake(self.bounds.origin.x,
-                                      self.bounds.origin.y,
-                                      self.bounds.size.width,
-                                      self.bounds.size.height + EXTENDED_EDGE_SIZE);
-    self.bounds = extendedBounds;;
-    
-    //move the text frame, image view and place holder up
-    self._textView.frame = CGRectMake(self._textView.frame.origin.x,
-                                      self._textView.frame.origin.y,
-                                      self._textView.bounds.size.width,
-                                      self._textView.bounds.size.height - EXTENDED_EDGE_SIZE);
-    
-    self.imageView.frame = CGRectMake(self.imageView.frame.origin.x,
-                                      self.imageView.frame.origin.y,
-                                      self.imageView.bounds.size.width,
-                                      self.imageView.bounds.size.height - EXTENDED_EDGE_SIZE);
-    
-    if (self.placeHolderView)
-    {
-        self.placeHolderView.frame = self.imageView.frame;
-    }
-    
-    //now add the UIView for placeholder to the end
-    if (self.controlView == nil)
-    {
-        UIView * controlView = [self createControlView];
-        [self addSubview:controlView];
-        self.controlView = controlView;
-        self.isControlViewShowing = YES;
-        
-    }
-}
-
--(UIView *) createControlView
-{
-    CGRect controlViewFrame = CGRectMake(self.imageView.frame.origin.x,
-                                         self.imageView.frame.origin.y + self.imageView.frame.size.height,
-                                         self.imageView.frame.size.width,
-                                         EXTENDED_EDGE_SIZE);
-    UIView * controlView = [[UIView alloc] initWithFrame:controlViewFrame];
-    controlView.backgroundColor = [UIColor colorWithWhite:0.9 alpha:1];
-    controlView.alpha = 0;
-    UIButton * button = [UIButton buttonWithType:UIButtonTypeSystem];
-    button.hidden = NO;
-    button.titleLabel.text = @"aklsjdalksjdlkajsdlkajslkdjalksdjas";
-    //I have no idea why the title hidden property gets set to YES when button is initiated
-    button.tintColor = [[ThemeFactory currentTheme] tintColor];
-    [button setTitle: HIDE_TEXT forState:UIControlStateNormal];
-    [button addTarget:self action:@selector(togglePressed:) forControlEvents:UIControlEventTouchUpInside];
-    [controlView addSubview:button];
-    [button sizeToFit];
-    button.frame = CGRectMake(controlView.frame.size.width/2 - button.frame.size.width/2,
-                              5,
-                              button.frame.size.width,
-                              button.frame.size.height);
-    button.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
-    return controlView;
-}
-
--(void) layoutControlElements
-{
-    //one day I should use autolayout programatically here to center it automatically
-   if (self.controlView)
-   {
-       NSArray * subViews = self.controlView.subviews;
-       if (subViews != nil && [subViews count] != 0)
-       {
-           UIButton * hideButton = subViews[0];
-           hideButton.frame = CGRectMake(self.controlView.frame.size.width/2 - hideButton.frame.size.width/2,
-                                     5,
-                                     hideButton.frame.size.width,
-                                     hideButton.frame.size.height);
-           hideButton.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
-       }
-   }
-}
-
--(void) togglePressed:(UIButton *) sender
-{
-    if ([sender.titleLabel.text isEqualToString:HIDE_TEXT])
-    {
-        //We just hide the place holder and text View
-        self._textView.hidden = YES;
-        self.placeHolderView.hidden = YES;
-        [sender setTitle:SHOW_TEXT forState:UIControlStateNormal];
-        [sender sizeToFit];
-        
-    }
-    else if ([sender.titleLabel.text isEqualToString:SHOW_TEXT])
-    {
-        self._textView.hidden = NO;
-        self.placeHolderView.hidden = NO;
-        [sender setTitle:HIDE_TEXT forState:UIControlStateNormal];
-        [sender sizeToFit];
-    }
-}
-
--(void) showPlaceHolder
-{
-    if (self.placeHolderView == nil)
-    {
-        if (!self.isControlViewShowing)
-        {
-            [self extendView];
-        }
-        self.placeHolderView = [[UIView alloc] initWithFrame:self.imageView.frame];
-        self.placeHolderView.backgroundColor = [UIColor whiteColor];
-        self.placeHolderView.alpha = 0.0;
-        [self insertSubview:self.placeHolderView belowSubview:self._textView];
-        
-        [UIView animateWithDuration:0.3 animations:^{
-            self.placeHolderView.alpha = 0.7;
-            self.controlView.alpha = 1;
-        }];
-    }
-}
-
--(void) hideAllControllerButtons
-{
-    if (self.controlView)
-    {
-        for(UIView * view in self.controlView.subviews)
-        {
-            view.hidden = YES;
-        }
-    }
-}
-
--(void) hidePlaceholder
-{
-    if (self.placeHolderView)
-    {
-        [self hideAllControllerButtons];
-        __weak ImageNoteView * weakSelf = self;
-        [UIView animateWithDuration:0.3 animations:^{
-            weakSelf.placeHolderView.alpha = 0;
-            
-            if (self.isControlViewShowing)
-            {
-                weakSelf.isControlViewShowing = NO;
-                weakSelf.frame = CGRectMake(self.frame.origin.x,
-                                            self.frame.origin.y,
-                                            self.frame.size.width,
-                                            self.frame.size.height - EXTENDED_EDGE_SIZE);
-                weakSelf.controlView.alpha = 0;
-                
-            }
-        }completion:^(BOOL finished){
-            [weakSelf.placeHolderView removeFromSuperview];
-            weakSelf.placeHolderView = nil;
-            [weakSelf.controlView removeFromSuperview];
-            weakSelf.controlView = nil;
-        }];
-    }
-}
-
--(void) textViewDidBeginEditing:(UITextView *)textView
-{
-    [self showPlaceHolder];
-    [super textViewDidBeginEditing:textView];
-}
-
--(void) textViewDidEndEditing:(UITextView *)textView
-{
-    if ([textView.text isEqualToString:@""])
-    {
-        if (self.placeHolderView) [self hidePlaceholder];
-    }
 }
 
 -(void) resizeToRect:(CGRect)rect Animate:(BOOL)animate
@@ -352,17 +145,4 @@
     
 }
 
--(void) textViewDidChange:(UITextView *)textView
-{
-    if ([textView.text isEqualToString:@""] &&
-        self.placeHolderView != nil)
-    {
-        [self hidePlaceholder];
-    }
-    else if (![textView.text isEqualToString:@""] &&
-             self.placeHolderView == nil)
-    {
-        [self showPlaceHolder];
-    }
-}
 @end
